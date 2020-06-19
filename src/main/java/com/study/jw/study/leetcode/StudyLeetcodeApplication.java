90be64f377631cc3d828fe8e99381e9ae50f9a85
root <jeffjiangwenwen_v@didiglobal.com>
2020-06-19 11:08:09 +0800

    init

---
+++ src/main/java/com/study/jw/study/leetcode/StudyLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.study.jw.study.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLeetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/jw/study/leetcode/StudyLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jw.study.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
